721748cf2d7450ca90672e2154611a64f2c84888
leoamoedo10 <leoamoedo10@gmail.com>
2020-11-11 14:31:16 -0300

    create pages 'landing' and 'routes'

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Iniciando o app!</Text>
+    <>
+      <Routes />
       <StatusBar style="dark" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './style';
+
+function Landing() {
+  return (
+    <View style={styles.container}>
+      <Text>Iniciando o app!</Text>
+    </View>
+  );
+}
+
+export default Landing;
