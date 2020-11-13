324aa8f92f857bfb7e6c4c9d622978e1672ab4aa
leoamoedo10 <leoamoedo10@gmail.com>
2020-11-13 14:41:38 -0300

    adding fonts

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
 
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import {Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins';
 
 import Routes from './src/routes';
 
@@ -10,6 +11,8 @@ export default function App() {
   let [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
   });
 
   if (!fontsLoaded) {
