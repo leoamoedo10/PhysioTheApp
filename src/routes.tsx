5356b2659f07a50829d4b801470f2a1a48190acd
leoamoedo10 <leoamoedo10@gmail.com>
2020-11-13 15:43:44 -0300

    create routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,15 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from './pages/Landing';
+import ExercicesPhysio from './pages/ExercicesPhysio';
+import styles from './pages/Landing/style';
 
 const {Navigator, Screen} = createStackNavigator();
 
-export default function Routes() {
+function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing}/>
+        <Screen name="ExercicesPhysio" component={ExercicesPhysio}/>
       </Navigator>
     </NavigationContainer>
   );
 }
+
+export default Routes;
